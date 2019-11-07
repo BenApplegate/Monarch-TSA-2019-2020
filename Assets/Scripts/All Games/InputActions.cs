210d439532f02731966743929807bfcf5be85899
BenApplegate <brapplegate01@bvsd.org>
2019-11-07 09:54:12 -0700

    Added enviornment and Camera Aiming to Archery

--- Assets/Scripts/All Games/InputActions.cs
+++ Assets/Scripts/All Games/InputActions.cs
@@ -13,9 +13,51 @@ public class InputActions : IInputActionCollection, IDisposable
     {
         asset = InputActionAsset.FromJson(@"{
     ""name"": ""InputActions"",
-    ""maps"": [],
+    ""maps"": [
+        {
+            ""name"": ""Archery"",
+            ""id"": ""6998fda7-6cc2-4a90-a087-16538326d249"",
+            ""actions"": [
+                {
+                    ""name"": ""Aim"",
+                    ""type"": ""Value"",
+                    ""id"": ""15b2ca23-bee5-4111-82ba-95754f99beb2"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""827076e7-daec-423a-93fe-ab643e5d29d8"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Aim"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""68e10f65-0b1b-40cc-b9e6-49ed85ce3642"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Aim"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
     ""controlSchemes"": []
 }");
+        // Archery
+        m_Archery = asset.FindActionMap("Archery", throwIfNotFound: true);
+        m_Archery_Aim = m_Archery.FindAction("Aim", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -60,5 +102,42 @@ public class InputActions : IInputActionCollection, IDisposable
     public void Disable()
     {
         asset.Disable();
+    }
+
+    // Archery
+    private readonly InputActionMap m_Archery;
+    private IArcheryActions m_ArcheryActionsCallbackInterface;
+    private readonly InputAction m_Archery_Aim;
+    public struct ArcheryActions
+    {
+        private InputActions m_Wrapper;
+        public ArcheryActions(InputActions wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Aim => m_Wrapper.m_Archery_Aim;
+        public InputActionMap Get() { return m_Wrapper.m_Archery; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(ArcheryActions set) { return set.Get(); }
+        public void SetCallbacks(IArcheryActions instance)
+        {
+            if (m_Wrapper.m_ArcheryActionsCallbackInterface != null)
+            {
+                Aim.started -= m_Wrapper.m_ArcheryActionsCallbackInterface.OnAim;
+                Aim.performed -= m_Wrapper.m_ArcheryActionsCallbackInterface.OnAim;
+                Aim.canceled -= m_Wrapper.m_ArcheryActionsCallbackInterface.OnAim;
+            }
+            m_Wrapper.m_ArcheryActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                Aim.started += instance.OnAim;
+                Aim.performed += instance.OnAim;
+                Aim.canceled += instance.OnAim;
+            }
+        }
+    }
+    public ArcheryActions @Archery => new ArcheryActions(this);
+    public interface IArcheryActions
+    {
+        void OnAim(InputAction.CallbackContext context);
     }
 }
---
+++ Assets/Scripts/Archery/CameraAim.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraAim : MonoBehaviour
+{
+
+    InputActions input; //Input object from the new Input System
+    Vector2 inputPos;
+    Quaternion StartRotation;
+
+    public int verticalMin;
+    public int verticalMax;
+    public int horizontalMin;
+    public int horizontalMax;
+
+    
+    private float horizontal;
+    private float vertical;
+
+    
+
+    private void Awake()
+    {
+        input = new InputActions();
+        input.Archery.Aim.performed += ctx => inputPos = ctx.ReadValue<Vector2>();
+        input.Archery.Aim.canceled += ctx => inputPos = Vector2.zero;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        StartRotation = this.transform.rotation;
+
+        transform.rotation = StartRotation;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(inputPos);
+        if(vertical >= verticalMin && inputPos.y < 0)
+        {
+            vertical += inputPos.y * .1f;
+            transform.Rotate(-inputPos.y * .1f, 0, 0, Space.World);
+        }
+        if (vertical <= verticalMax && inputPos.y > 0)
+        {
+            vertical += inputPos.y * .1f;
+            transform.Rotate(-inputPos.y * .1f, 0, 0, Space.World);
+        }
+
+
+        if (horizontal <= horizontalMax && inputPos.x > 0)
+        {
+            Debug.Log("Right");
+            horizontal += inputPos.x * .1f;
+            transform.Rotate(0, inputPos.x * .1f, 0, Space.Self);
+        }
+        if (horizontal >= horizontalMin && inputPos.x < 0)
+        {
+            Debug.Log("Left");
+            horizontal += inputPos.x * .1f;
+            transform.Rotate(0, inputPos.x * .1f, 0, Space.Self);
+        }
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();  //Enables controlls
+    }
+
+    private void OnDisable()
+    {
+        input.Disable(); //Disables controlls if object gets disabled
+    }
+}
