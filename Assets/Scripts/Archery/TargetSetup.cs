9c2e4096c3638979267dd89db59d487c419b300b
GloriousChonk <57465150+GloriousChonk@users.noreply.github.com>
2019-11-06 20:59:57 -0700

    Added Target and Target Placement Code

---
+++ Assets/Scripts/Archery/TargetSetup.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class TargetSetup : MonoBehaviour
+{
+    public Transform player; // Allows reference to player position
+    public float minDist; // Sets minimum distance from the player
+    public float maxDist; // Sets maximum distance from the player
+    public float range; // Holds the RNG for the placement of the target
+    public Vector3 placement; // Used for placing target at random distance between minimum and maximum distance
+
+    // Start is called before the first frame update
+    // Initializes target distance from player
+    void Start()
+    {
+        range = Random.Range(minDist, maxDist);
+        placement.z += range;
+        transform.position = player.position + placement;
+    }
+
+    
+    void fixedUpdate()
+    {
+        
+    }
+}
