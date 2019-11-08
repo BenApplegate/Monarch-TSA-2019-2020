0823370a68508528892ab1a99b870fffa3deeeed
BenApplegate <applegateben03@gmail.com>
2019-11-07 20:59:33 -0700

    Adjusted Settings

---
+++ Assets/Scripts/Archery/Arrow.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        this.GetComponent<Rigidbody>().isKinematic = true;
+    }
+}
