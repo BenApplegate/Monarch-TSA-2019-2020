d1a26890c080478cc818fa80169a928d406656bb
GrupMusicBot <42746052+GrupMusicBot@users.noreply.github.com>
2019-11-20 20:25:15 -0700

    Added ProgressBar
    Added Sprites for ProgrssBar

---
+++ Assets/Assets/Weight Lifting/DetectInput.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("This is a test");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
