70b8e8ec3f70d7899701b98a06e3b6d9f22d21a1
skyatron <37194260+skyatron@users.noreply.github.com>
2020-01-06 20:34:50 -0700

    Error in shotgun script
    There is an error that I can't seem to fix, : error CS1061: 'Camera' does not contain a definition for 'tranform' and no accessible extension method 'tranform' accepting a first argument of type 'Camera' could be found (are you missing a using directive or an assembly reference?). If someone could fix the ray casting that would be great.

---
+++ Assets/Scripts/Shooting/Shotgun.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Shotgun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.tranform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Shooting/Target.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
