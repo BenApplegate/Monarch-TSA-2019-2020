599b88922944508fcc14358e9f08d9abada7426b
GrupMusicBot <42746052+GrupMusicBot@users.noreply.github.com>
2020-01-08 21:05:57 -0700

    Made Main Menu
    Added a Main Menu when the game boots
    
    - When you press play it goes straight to Archery
    - Options currently only has a toggleable Fullscreen
    - Quit does exactly what it says... Closes the game

---
+++ Assets/OptionsMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+
+    public void SetVolume (float volume) { // Set Volume to the Game, Audio Slider not yet made
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetFullScreen (bool isFullScreen) // Toggles Fullscreen, window not resizeable. But has a way of exiting
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+}
