45d565e31663de0f27ff3c04eda82d1732c76a01
skyatron <37194260+skyatron@users.noreply.github.com>
2020-01-08 22:42:37 -0700

    finished shooting

--- Assets/Scripts/Shooting/Target.cs
+++ Assets/Scripts/Shooting/Target.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     public float health = 50f;
+    float x;
+    float y;
+    float z;
+    Vector3 pos;
+    void Start()
+    {
+        x = Random.Range(-5, 5);
+        y = Random.Range(3, 7);
+        z = 5;
+        pos = new Vector3(x, y, z);
+        transform.position = pos;
+    }
+
 
     public void TakeDamage(float amount)
     {
