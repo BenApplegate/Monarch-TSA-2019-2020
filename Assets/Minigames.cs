1434e827ddadc4e856911bdf6823dc5aaeb14f0c
GrupMusicBot <42746052+GrupMusicBot@users.noreply.github.com>
2020-01-09 21:33:29 -0700

    All Scenes in MainMenu
    If "Monarch TSA" text is clicked, you are brought to a menu with all the games to test, made to test the full game easier

---
+++ Assets/Minigames.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Minigames : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void archeryGame () {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void shootingGame () {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    
+    public void swimmingGame () {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+    public void weightliftingGame () {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+    }
+}
