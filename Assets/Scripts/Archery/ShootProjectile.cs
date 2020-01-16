1c040a3df9659e5f9d743acd4f0a6076ecfec6aa
BenApplegate <applegateben03@gmail.com>
2020-01-16 08:54:48 -0700

    Commented code and removed un-needed files

--- Assets/Scripts/Archery/ShootProjectile.cs
+++ Assets/Scripts/Archery/ShootProjectile.cs
@@ -19,29 +19,29 @@ public class ShootProjectile : MonoBehaviour
     public GameObject Arrow; // the object that is used as the arrow
 
     public bool zoom = true; // shows if the camera zooms when 
-    float startingFOV;
-    public Camera cam;
-    public float zoomFactor = 2;
+    float startingFOV; // The FOV that the camera starts with
+    public Camera cam; // the camera
+    public float zoomFactor = 2; // the factor that the camera zooms by
 
     private void Awake()
     {
             input = new InputActions();
-            input.Archery.FireArrow.performed += ctx => inputStrength = ctx.ReadValue<float>();
-            input.Archery.FireArrow.performed += ctx => fireing = true;
-            input.Archery.FireArrow.canceled += ctx => fireing = false;
-            input.Archery.FireArrow.canceled += ctx => Fire();
+            input.Archery.FireArrow.performed += ctx => inputStrength = ctx.ReadValue<float>(); // stores the current value of the fire button
+            input.Archery.FireArrow.performed += ctx => fireing = true; // stores that the player is fireing
+            input.Archery.FireArrow.canceled += ctx => fireing = false; // stores that the player is no longer fireing
+            input.Archery.FireArrow.canceled += ctx => Fire(); // fires the arrow when the button is released
         
     }
 
     private void BuildStrength()
     {
-        if(strength < maxStrength)
+        if(strength < maxStrength) // if current strenghth is less then the max strength
         {
-            strength += inputStrength * strengthBuildRate * Time.deltaTime;
+            strength += inputStrength * strengthBuildRate * Time.deltaTime; // builds strength
         }
-        if (zoom)
+        if (zoom) // if camera is supposed to zoom
         {
-            cam.fieldOfView = startingFOV - (strength*zoomFactor);
+            cam.fieldOfView = startingFOV - (strength*zoomFactor); // zooms camera based on strength
         }
     }
 
@@ -49,15 +49,15 @@ public class ShootProjectile : MonoBehaviour
     {
 
 
-        cam.fieldOfView = startingFOV;
-        if (arrowsShot < arrows) { 
-        GameObject fired = Instantiate(Arrow, this.transform.position, this.transform.rotation);
-        fired.transform.Rotate(90, 0, 0);
-        fired.GetComponent<Rigidbody>().velocity += this.transform.forward * strength * power;
-        fired.GetComponent<Arrow>().shootProjectile = this;
-        strength = 0;
-        arrowsShot++;
-        if(arrowsShot == arrows)
+        cam.fieldOfView = startingFOV; // resets camera FOV
+        if (arrowsShot < arrows) { //if you have arrows left to fire
+        GameObject fired = Instantiate(Arrow, this.transform.position, this.transform.rotation); //creates an arrow and saves it into the reference fired
+        fired.transform.Rotate(90, 0, 0); // Rotates the arrow so it faces the right way
+        fired.GetComponent<Rigidbody>().velocity += this.transform.forward * strength * power; // gives the arrow speed
+        fired.GetComponent<Arrow>().shootProjectile = this; // Gives the arrow a reference to this script
+        strength = 0; // resets strength
+        arrowsShot++; // increments arrowsShot
+        if(arrowsShot == arrows) // If the player has fired all of their arrows
             {
                 endGame();
             }
@@ -66,13 +66,13 @@ public class ShootProjectile : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        startingFOV = cam.fieldOfView;
+        startingFOV = cam.fieldOfView; // sets starting FOV
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (fireing && arrowsShot<arrows)
+        if (fireing && arrowsShot<arrows) // If you have arrows to fire and you are holding the fire button
         {
             BuildStrength();
         }
