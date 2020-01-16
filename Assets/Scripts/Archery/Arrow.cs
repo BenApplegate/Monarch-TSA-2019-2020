f7ebd998085afa1ed26af90b1575597b4ee1ecd7
BenApplegate <applegateben03@gmail.com>
2020-01-16 09:57:18 -0700

    added target

--- Assets/Scripts/Archery/Arrow.cs
+++ Assets/Scripts/Archery/Arrow.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Arrow : MonoBehaviour
 {
     public ShootProjectile shootProjectile; // This is broken
+    public CapsuleCollider otherColider;
 
     public Arrow(ShootProjectile shootProjectile) // This is broken
     {
@@ -14,5 +15,7 @@ public class Arrow : MonoBehaviour
     private void OnTriggerEnter(Collider collision)
     {
         this.GetComponent<Rigidbody>().isKinematic = true;
+        otherColider.enabled = false;
+        Debug.Log(collision.name);
     }
 }
