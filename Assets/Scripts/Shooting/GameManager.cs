a2981f5b9f7b56e20a93f0fcc9a45794a65569cb
skyatron <37194260+skyatron@users.noreply.github.com>
2020-01-18 15:39:25 -0700

    made it so that the pigeons yeet

---
+++ Assets/Scripts/Shooting/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Launcher skeet1;
+    public Launcher skeet2;
+    public int power;
+    public bool fire1 = false;
+    public bool fire2 = false;
+    public bool fire3 = false;
+    public bool fire4 = false;
+    private void Update()
+    {
+        Debug.Log(Time.time);
+            if (Time.time >= 5 && !fire1)
+            {
+                skeet2.yeetSkeet(power);
+            fire1 = true;
+            }
+            if (Time.time >= 10 && !fire2)
+            {
+                skeet1.yeetSkeet(power);
+            fire2 = true;
+            }
+            if (Time.time >= 15 && !fire3)
+            {
+                skeet2.yeetSkeet(power);
+            fire3 = true;
+            }
+            if (Time.time >= 20 && !fire4)
+            {
+                skeet1.yeetSkeet(power);
+            fire4 = true;
+            }
+        
+    }
+    private void Start()
+    {
+        skeet1.yeetSkeet(power);
+        
+    }
+}
---
+++ Assets/Scripts/Shooting/Launcher.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    public GameObject skeet;
+    // Start is called before the first frame update
+    public void yeetSkeet(float firespeed)
+    {
+            GameObject Skeet2Yeet = Instantiate(skeet, this.transform.position, this.transform.rotation);
+            Skeet2Yeet.GetComponent<Rigidbody>().velocity = Skeet2Yeet.transform.forward * firespeed;
+    }
+}
