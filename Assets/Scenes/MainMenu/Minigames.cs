bb16ca5a6fdb1935154c195ddf6c1ef9e56244a3
GrupTheMusicBot "Kuebjii <bobwthomp@gmail.com>
2020-01-18 12:06:15 -0700

    MainMenu Fixes and Updates
    Added Gamelist to MainMenu
    Added Highscore to main Menu
    And added more stuff

--- Assets/Scenes/MainMenu/Minigames.cs
+++ Assets/Scenes/MainMenu/Minigames.cs
@@ -2,22 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
 public class Minigames : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    public TextMeshProUGUI archeryHighScore;
+    
+    private void Start(){
+        if(PlayerPrefs.HasKey("ArcheryHighScore")){
+        archeryHighScore.SetText("High Score: " + PlayerPrefs.GetInt("ArcheryHighScore"));
+        }
+        else{
+            archeryHighScore.SetText("High Score: 0");
+        }
+    }
+
     public void archeryGame () {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Archery");
     }
 
     public void shootingGame () {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        SceneManager.LoadScene("Shooting");
     }
     
     public void swimmingGame () {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+        SceneManager.LoadScene("Swimming");
     }
 
     public void weightliftingGame () {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+        SceneManager.LoadScene("Weight Lifting");
+    }
+
+    public void rowingGame () {
+        SceneManager.LoadScene("Rowing");
     }
 }
