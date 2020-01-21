e71989c6cbd28ab2a2fda658dbf34309f7e15eac
BenApplegate <applegateben03@gmail.com>
2020-01-20 20:44:52 -0700

    Fixed Audio Duplication and moved song credit

--- Assets/Scenes/MainMenu/MainMenuScripts/AudioDontDestroy.cs
+++ Assets/Scenes/MainMenu/MainMenuScripts/AudioDontDestroy.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class AudioDontDestroy : MonoBehaviour
 {
+    public static AudioDontDestroy instance = null;
     private AudioSource _audioSource;
     private void Awake()
     {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if(instance!= this)
+        {
+            Destroy(gameObject);
+        }
         DontDestroyOnLoad(transform.gameObject);
         _audioSource = GetComponent<AudioSource>();
     }
