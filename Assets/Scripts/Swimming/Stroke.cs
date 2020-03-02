5b49f5af5a0a88ac4b7802ff72d89572e61991db
GloriousChonk <57465150+GloriousChonk@users.noreply.github.com>
2020-03-01 18:33:48 -0700

    Updated the swimming script *incomplete*

--- Assets/Scripts/Swimming/Stroke.cs
+++ Assets/Scripts/Swimming/Stroke.cs
@@ -6,18 +6,17 @@ public class Stroke : MonoBehaviour
 {
     public Rigidbody move; // Holds the position of the player for movement
     public Transform pos;
-    private bool apressed; // Signals if "a" key has been pressed
-    private bool dpressed; // Signals if "d" key has been pressed
     public float strokeStrength = 0.1f;
+    public int air = 100; // Will be consumed through player actions, determines end
+
     SceneManager SceneManager = new SceneManager();
     float time;
-   
+
 
     // Start is called before the first frame update
     void Start()
     {
-        apressed = true;
-        dpressed = true;
+        
     }
 
     // Update is called once per frame
@@ -28,36 +27,14 @@ public class Stroke : MonoBehaviour
             FindObjectOfType<SwimmingScoreManager>().EndGame();
         }
 
-        if (dpressed == true)
-        {
-            CheckA();
-        }
-        else if (apressed == true)
-        {
-            CheckD();
-        }
-        
+        Actions();
     }
 
-    public void CheckA()
+    private void Actions()
     {
-        if (Input.GetKeyDown(KeyCode.A) == true)
+        if (Input.GetKeyDown("a") == true)
         {
-            move.AddForce(Vector3.right * strokeStrength);
-            apressed = true;
-            dpressed = false;
             
         }
-    } // Checks if A is pressed, and if so, moves the player
-
-    public void CheckD()
-    {
-        if (Input.GetKeyDown(KeyCode.D) == true)
-        {
-            move.AddForce(Vector3.right * strokeStrength);
-            dpressed = true;
-            apressed = false;
-            
-        }
-    } // Checks if D is pressed, and if so, moves the player
+    }
 }
