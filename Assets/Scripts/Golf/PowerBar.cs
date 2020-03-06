feea5d60ed9a24445518b9e931a75d8e0ab6d372
GrupTheMusicBot "Kuebjii <bobwthomp@gmail.com>
2020-03-05 19:39:33 -0700

    A mega bad powerbar
    shme me

---
+++ Assets/Scripts/Golf/PowerBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerBar : MonoBehaviour
+{
+    public float maxBar = 100f;
+    public float startBar = 0f;
+    public float increaseBar = 5f;
+    public Slider slider;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            slider.value = increaseBar;
+        }
+    }
+}
